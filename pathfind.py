996ff7d41ea06561d369b2333adcb00d91e7be68
Jasper den Ouden <o.jasper@gmail.com>
2014-06-11 02:14:34 +0200

    Started pathfind, needed for an idea to reputation

---
+++ pathfind.py
@@ -0,0 +1,72 @@
+##  Copyright (C) 11-06-2014 Jasper den Ouden.
+##
+##  This is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published
+##  by the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+
+# Generic pathfind.
+
+from heapq import heapify, heappush, heappop  # Heap, used as priority list.
+
+g_run_i = 0
+
+class PathedNode:  # Dijkstras algorithm for finding paths.
+    def __init__(self, data, edges=None):
+        self.data = data
+        self.edges = edges or []
+        self.costval = 0  # Values used for the pathfinding.
+        self.run_i   = 0
+
+    def cost(self, to, goal):  # Cost between two positions.
+        return 1
+
+    def step_from(self, heap, at_cost, run_i, goal):
+        self.costval = at_cost
+        self.run_i   = run_i
+        if self is goal:  # Found it, return it!
+            return goal
+        
+        for el in self.edges:
+            if el.run_i != run_i:  # Add if wasnt already there.
+                heappush(heap, (at_cost + self.cost(el, goal), el))
+        return None
+
+    # Pathfind from the node itself to the goal.
+    def pathfind_prep(self, goal, run_i):
+        heap = []
+        got = self.step_from(heap, 0, run_i, goal)
+        while got is None:  # Keep picking the lowest total cost.
+            try:
+                cost, cur = heappop(heap)
+            except IndexError:
+                return False  # Could not find a path.
+            got = cur.step_from(heap, cost, run_i, goal)
+        return True
+
+    def pathfind_track(self, goal, run_i):
+        cur = self
+        path = []
+        while cur is not goal:
+            first = True
+            min_costval, nxt = 0, None
+            for el in cur.edges:
+                if el.run_i == run_i and (el.costval < min_costval or first):
+                    min_costval = el.costval
+                    nxt = el
+            path.append(cur)
+            cur = nxt
+            assert cur is not None  # Otherwise pathfind_prep returns False.
+        path.append(cur)
+        return path
+
+    # Pathfinds from self to goal, returns None if no path, the backtrack otherwise.
+    # goal.costval will contain the amount it costs.
+    def pathfind(self, goal, run_i=None):
+        if run_i is None:  # Note all nodes will have to be reset at some point.
+            global g_run_i
+            g_run_i += 1
+            run_i = g_run_i
+        if self.pathfind_prep(goal, run_i):
+            return self.pathfind_track(goal, run_i)
---
+++ test/test_pathfind.py
@@ -0,0 +1,49 @@
+import os.path
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from pathfind import PathedNode
+
+def connect_chain(to_chain):
+    for i in range(len(to_chain)-1):
+        to_chain[i].edges.append(to_chain[i+1])
+    return to_chain
+
+def make_chain(n):
+    return connect_chain(map(PathedNode, range(n)))
+
+def test_chain(n):
+    chain = make_chain(n)
+    for k2 in range(n - 1):  #Overly exhaust stupidly identical possibilities.
+        for k1 in range(n - k2):
+            path = chain[k1].pathfind(chain[-k2-1])
+            assert len(path) == n - k2 - k1
+   # Cannot go backward.
+    assert chain[-1].pathfind(chain[0]) is None
+
+test_chain(20)
+
+def ensure_node(name, nodes):
+    if name not in nodes:
+        nodes[name] = PathedNode(name)
+    return nodes[name]
+
+#Creates netowrk by bunches of cains.
+def create_network(lists, nodes=None):
+    nodes = nodes or {}
+    for chain in lists:
+        for i in range(len(chain)-1):
+            cur = ensure_node(chain[i], nodes)
+            nxt = ensure_node(chain[i+1], nodes)
+            if nxt not in cur.edges:
+                cur.edges.append(nxt)
+    return nodes
+
+def listnames(nodes):
+    return map(lambda(el): el.data, nodes)
+
+net = create_network([[1,2,3,4,5,6,7,8,9], [3,'a', 'b', 7]])
+# Thou shalt take the shortcut.
+assert listnames(net[1].pathfind(net[9])) == [1,2,3,'a','b',7,8,9]
+
