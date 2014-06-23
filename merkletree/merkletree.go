012e6d9de6c85ba4519eda52c77e8abdd6e50de7
Jasper den Ouden <o.jasper@gmail.com>
2014-06-23 19:25:07 +0200

    No idea if this will work, but feeling fairly good about it. Next, verifier, and testing on some randomly generated stuff

--- merkletree/merkletree.go
+++ merkletree/merkletree.go
@@ -9,56 +9,93 @@ import (
 
 //NOTE: Hash64 is too few bits imo...
 
-type MerkleTreePortion struct { //Subtree in there somewhere.
-	hash  [sha256.Size]byte
-	n     int
-}
-
 // A node of Merkle tree, note that the below omits a lot.
 type MerkleNode struct {
-	hash   []byte
+	hash   [sha256.Size]byte
 	left   *MerkleNode
 	right  *MerkleNode
-	interesting  bool
+	up     *MerkleNode  // Otherwise it is messy to create the path.
+	interest  bool
 }
+
+func new_MerkleNode(left *MerkleNode,right *MerkleNode) *MerkleNode {
+	interest := left.interest || right.interest	
+	hash = sha256.Sum256(append(left.hash, right.hash...))
+	node = &MerkleNode{hash:hash, left:left, right:right, up:nil, interest:interest}
+	left.up  = node
+	right.up = node
+	return node
+}
+
+func selective_new_MerkleNode(left *MerkleNode,right *MerkleNode) *MerkleNode {
+	node = new_MerkleNode(left, right)
+	node.left.effect_interest()
+	node.right.effect_interest()
+	return node
+}
+
+func (node MerkleNode) effect_interest()
+{
+	if !node.interest { //No interest in branches.
+		node.left = nil
+		node.right = nil
+	}
+}
+
+type MerkleTreePortion struct { //Subtree in there somewhere.
+	node   *MerkleNode
+	depth  int
+}
+
 
 //The algo is to make a 'mountain range'(forgot link)
-// and put the latest together if we have them. 
+// and put the latest together if we have them. Meanwhile, it creates a tree, but
+// drops anything in which there is no interest.
 type MerkleTreeGen struct {
 	list  []MerkleTreePortion
-	tree  *MerkleNode
 }
 
-//Get the twig part of the merkle path of the _next_ one.
-func (gen MerkleTreeGen) merkletwig() [][sha256.Size]byte {
-	list := [][sha256.Size]byte
-	for i, p := range gen.list {
-		list = append(list, p.hash)
-	}
-	return list
-}
-
-func (gen MerkleTreeGen) addchunk(chunk []byte, interest bool) {
+// Adds chunk, returning the leaf the current is on.
+func (gen MerkleTreeGen) add_chunk(chunk []byte, interest bool) *MerkleNode {
 	h := sha256.Sum256(chunk)
-	if len(gen.list) == 0 || gen.list[0].n != 1 {
-		gen.list = append(gen.list, MerkleTreePortion{hash:h, n:1})
-	} else { //Combine the first one.
-		// assert gen.list[0].n == 1
-		combined := append(h, gen.list[0].hash...)
-		gen.list[0].hash = sha256.Sum256(combined)
-		// And potentially combine more.
-		for len(gen.list) >= 2 && gen.list[1].n == gen.list[0].n {
-			combined := append(gen.list[0].hash, gen.list[1].hash...)
+	if len(gen.list) == 0 || gen.list[0].depth != 1 {
+		add_node := &MerkleNode{hash:h, left:nil, right:nil, up:nil, interest:false}
+		gen.list = append(gen.list, MerkleTreePortion{node:add_node, n:1})
+		return add_node
+	} else {
+		// assert gen.list[0].depth == 1
+		new_leaf = MerkleNode{hash:h, left:nil, right:nil, interest:interest}
+
+		new_node := selective_new_MerkleNode(new_leaf, gen.list[0].node)  //Combine the two.
+		gen.list[0] = MerkleTreePortion{node:new_node, depth:2}
+
+		// Combine more, while equal depth.
+		for len(gen.list) >= 2 && gen.list[1].depth == gen.list[0].depth {
+			new_node := selective_new_MerkleNode(gen.list[0], gen.list[1])
 			gen.list = gen.list[1:] // Cut off the first one.
-			gen.list[0].hash = sha256.Sum256(combined)  //(n is good).
+			gen.list[0] = new_node
 		}
+		return new_leaf  //Return the leaf.
 	}
 }
 
-//Coerce the last parts together.
-func (gen MerkleTreeGen) finish(){
-	//TODO
+// Coerce the last parts together, returning the root.
+func (gen MerkleTreeGen) finish() MerkleNode* {
+	// assert len(gen.list) > 0
+	for len(gen.list) >= 2  {
+		new_node = selective_new_MerkleNode(gen.list[0], gen.list[1])
+		gen.list = gen.list[1:]
+		gen.list[0] = new_node
+	}
+	return gen.list[0].node
 }
 
-
-func gen_path(chunk []byte)
+// Calculate the path, requires that you have calculated it up to the root.
+func (node *MerkleNode) gen_path() [][sha256.Size]byte {
+	if node.up != nil {
+		return append(node.hash, node.up.gen_path()...)
+	} else {
+		var ret [][sha256.Size]byte = {}
+		return ret
+	}
+}
--- merkletree/readme.md
+++ merkletree/readme.md
@@ -24,3 +24,6 @@ The Ethereum contract merely has to check.
 An idea is to take probabilities for chunks and make a somewhat lobsided tree
 that minimizes the average length of proving merkle paths. The checking mechanism
 doesnt care about the shape of the merkle tree, so it can be added later.
+
+On the other hand, off-chain, the efficiency in memory is better if things you want
+to keep track of are bunched together.
